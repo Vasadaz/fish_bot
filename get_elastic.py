bf6a3030f47cd3959debf4d0bab4ea8781544426
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-29 22:07:04 +0300

    Testing elasticpath.

--- get_elastic.py
+++ get_elastic.py
@@ -11,53 +11,35 @@ ELASTIC_CLIENT_ID = env.str('ELASTIC_CLIENT_ID')
 ELASTIC_CLIENT_SECRET = env.str('ELASTIC_CLIENT_SECRET')
 ELASTIC_STORE_ID = env.str('ELASTIC_STORE_ID')
 
-
 data = {
     'client_id': ELASTIC_CLIENT_ID,
     'client_secret': ELASTIC_CLIENT_SECRET,
     'grant_type': 'client_credentials',
 }
 
+response_auth = requests.post(ELASTIC_BASE_URL + '/oauth/access_token', data=data)
+response_auth.raise_for_status()
+response_auth = response_auth.json()
+print(response_auth)
 
-response = requests.post(ELASTIC_BASE_URL + '/oauth/access_token', data=data)
-response.raise_for_status()
-response = response.json()
-print(response)
-
-headers = {
-    'Authorization': f'{response["token_type"]} {response["access_token"]}',
+headers_auth = {
+    'Authorization': f'{response_auth["token_type"]} {response_auth["access_token"]}',
 }
 
-
-
-response = requests.get(ELASTIC_BASE_URL + '/pcm/products', headers=headers)
-response.raise_for_status()
-#print(response.json())
+response_products = requests.get(ELASTIC_BASE_URL + '/pcm/products', headers=headers_auth)
+response_products.raise_for_status()
+print(response_products.json())
 
 
 data = {
-    'client_id': ELASTIC_CLIENT_ID,
-    'grant_type': 'implicit',
+    "bundle_configuration": {
+        "selected_options": {
+               "d298429b-a17d-45da-aca5-dac2c5abb604": 1
+        }
+
+    }
 }
 
-response = requests.post(ELASTIC_BASE_URL + '/oauth/access_token', data=data)
-response.raise_for_status()
-print(response.json())
-response = response.json()
-
-
-headers = {
-    'Authorization': f'{response["token_type"]} {response["access_token"]}',
-    'Content-Type': 'application/json',
-}
-
-data = {
-    "type": "token",
-    "email": "ron@swanson.com",
-    "password": "mysecretpassword",
-    "authentication_mechanism": "password"
-}
-
-response = requests.post(ELASTIC_BASE_URL + '/v2/customers/tokens', data=data, headers=headers)
+response = requests.post(ELASTIC_BASE_URL + '/v2/carts', data=data, headers=headers_auth)
 response.raise_for_status()
 
