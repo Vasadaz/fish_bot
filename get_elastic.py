79c838abf8e653841298ff52c8e5e8a629531ff1
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-29 23:11:29 +0300

    Testing elasticpath.

--- get_elastic.py
+++ get_elastic.py
@@ -11,6 +11,7 @@ ELASTIC_CLIENT_ID = env.str('ELASTIC_CLIENT_ID')
 ELASTIC_CLIENT_SECRET = env.str('ELASTIC_CLIENT_SECRET')
 ELASTIC_STORE_ID = env.str('ELASTIC_STORE_ID')
 
+
 data = {
     'client_id': ELASTIC_CLIENT_ID,
     'client_secret': ELASTIC_CLIENT_SECRET,
@@ -20,26 +21,64 @@ data = {
 response_auth = requests.post(ELASTIC_BASE_URL + '/oauth/access_token', data=data)
 response_auth.raise_for_status()
 response_auth = response_auth.json()
-print(response_auth)
+#print(response_auth)
 
 headers_auth = {
     'Authorization': f'{response_auth["token_type"]} {response_auth["access_token"]}',
 }
 
-response_products = requests.get(ELASTIC_BASE_URL + '/pcm/products', headers=headers_auth)
+
+response_products = requests.get(ELASTIC_BASE_URL + '/catalog/products', headers=headers_auth)
 response_products.raise_for_status()
-print(response_products.json())
+response_products = response_products.json()
+#print(response_products)
 
+product_1 = response_products['data'][0]['id']
+product_2 = response_products['data'][1]['id']
+product_3 = response_products['data'][2]['id']
 
-data = {
-    "bundle_configuration": {
-        "selected_options": {
-               "d298429b-a17d-45da-aca5-dac2c5abb604": 1
-        }
+response_product_1 = requests.get(ELASTIC_BASE_URL + f'/catalog/products/{product_1}', headers=headers_auth)
+response_product_1.raise_for_status()
+response_product_1 = response_product_1.json().get('data').get('attributes')
+print(response_product_1)
 
-    }
+response_product_2 = requests.get(ELASTIC_BASE_URL + f'/catalog/products/{product_1}', headers=headers_auth)
+response_product_2.raise_for_status()
+response_product_2 = response_product_2.json().get('data').get('attributes')
+print(response_product_2)
+
+response_product_3 = requests.get(ELASTIC_BASE_URL + f'/catalog/products/{product_1}', headers=headers_auth)
+response_product_3.raise_for_status()
+response_product_3 = response_product_3.json().get('data').get('attributes')
+print(response_product_3)
+
+client_id = 323232
+
+response_cart = requests.get(ELASTIC_BASE_URL + f'/v2/carts/{client_id}', headers=headers_auth)
+response_cart.raise_for_status()
+print(response_cart.json())
+
+headers_cart = {
+    'Authorization': f'{response_auth["token_type"]} {response_auth["access_token"]}',
+    'Content-Type': 'application/json',
 }
 
-response = requests.post(ELASTIC_BASE_URL + '/v2/carts', data=data, headers=headers_auth)
+data_1 = {
+            'data': {
+                'type': 'custom_item',
+                'name': response_product_1.get('name'),
+                'sku': response_product_1.get('sku'),
+                'description': response_product_1.get('description', 'Нет описания'),
+                'quantity': 5,
+                'price': {
+                    'amount': response_product_1.get('price').get('USD').get('amount'),
+                },
+            },
+        }
+
+response = requests.post(ELASTIC_BASE_URL + f'/v2/carts/{client_id}/items', json=data_1, headers=headers_cart)
 response.raise_for_status()
 
+response_cart = requests.get(ELASTIC_BASE_URL + f'/v2/carts/{client_id}/items', headers=headers_auth)
+response_cart.raise_for_status()
+print(response_cart.json())
