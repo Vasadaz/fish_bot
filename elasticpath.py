7ef50b1879786138f65ef5e7c92cc5f21eb2f5fb
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-07-04 21:33:20 +0300

    Reformatted code.

--- elasticpath.py
+++ elasticpath.py
@@ -53,67 +53,6 @@ class ElasticPath:
             'main_image_id': product_relationships.get('main_image').get('data').get('id'),
         }
 
-    def get_products(self) -> list[dict[str:str|int]]:
-        response = requests.get(self.products_url, headers=self.headers)
-        response.raise_for_status()
-
-        products = []
-        for product_notes in response.json().get('data'):
-            products.append(self._serialize_product_notes(product_notes))
-
-        return products
-
-    def get_product_notes(self, product_id) -> dict[str:str]:
-        response = requests.get(self.products_url + product_id, headers=self.headers)
-        response.raise_for_status()
-
-        return self._serialize_product_notes(response.json().get('data'))
-
-    def get_image_path(self, image_id) -> str:
-        dir_path = Path('images')
-        dir_path.mkdir(parents=True, exist_ok=True)
-
-        for file_path in dir_path.iterdir():
-            if image_id == file_path.stem:
-                return file_path.as_posix()
-
-        response = requests.get(self.files_url + image_id, headers=self.headers)
-        response.raise_for_status()
-        download_url = response.json().get('data').get('link').get('href')
-        save_path = Path(dir_path) / Path(download_url).name
-
-        download = requests.get(download_url)
-        download.raise_for_status()
-
-        with open(save_path, 'wb') as file:
-            file.write(download.content)
-
-        return save_path.as_posix()
-
-    def get_cart_items(self) -> dict[str:str]:
-        response = requests.get(self.carts_url + f'{self.client_id}/items', headers=self.headers)
-        response.raise_for_status()
-        cart_items = {
-            'cart_amount': response.json().get('meta').get('display_price').get('with_tax').get('amount'),
-            'products': [],
-        }
-
-        for item_notes in response.json().get('data'):
-            cart_items['products'].append({
-                'id': item_notes.get('id'),
-                'name': item_notes.get('name'),
-                'quantity': item_notes.get('quantity'),
-                'amount': item_notes.get('value').get('amount'),
-            })
-
-        return cart_items
-
-
-    def delete_product_from_cart(self, product_id) -> None:
-        response = requests.delete(self.carts_url + f'{self.client_id}/items/' + product_id, headers=self.headers)
-        response.raise_for_status()
-
-
     def add_product_to_cart(self, product_id, quantity) -> None:
         headers = {
             **self.headers,
@@ -141,6 +80,65 @@ class ElasticPath:
             json=product_data
         )
         response.raise_for_status()
+
+    def delete_product_from_cart(self, product_id) -> None:
+        response = requests.delete(self.carts_url + f'{self.client_id}/items/' + product_id, headers=self.headers)
+        response.raise_for_status()
+
+    def get_cart_items(self) -> dict[str:str]:
+        response = requests.get(self.carts_url + f'{self.client_id}/items', headers=self.headers)
+        response.raise_for_status()
+        cart_items = {
+            'cart_amount': response.json().get('meta').get('display_price').get('with_tax').get('amount'),
+            'products': [],
+        }
+
+        for item_notes in response.json().get('data'):
+            cart_items['products'].append({
+                'id': item_notes.get('id'),
+                'name': item_notes.get('name'),
+                'quantity': item_notes.get('quantity'),
+                'amount': item_notes.get('value').get('amount'),
+            })
+
+        return cart_items
+
+    def get_image_path(self, image_id) -> str:
+        dir_path = Path('images')
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        for file_path in dir_path.iterdir():
+            if image_id == file_path.stem:
+                return file_path.as_posix()
+
+        response = requests.get(self.files_url + image_id, headers=self.headers)
+        response.raise_for_status()
+        download_url = response.json().get('data').get('link').get('href')
+        save_path = Path(dir_path) / Path(download_url).name
+
+        download = requests.get(download_url)
+        download.raise_for_status()
+
+        with open(save_path, 'wb') as file:
+            file.write(download.content)
+
+        return save_path.as_posix()
+
+    def get_product_notes(self, product_id) -> dict[str:str]:
+        response = requests.get(self.products_url + product_id, headers=self.headers)
+        response.raise_for_status()
+
+        return self._serialize_product_notes(response.json().get('data'))
+
+    def get_products(self) -> list[dict[str:str|int]]:
+        response = requests.get(self.products_url, headers=self.headers)
+        response.raise_for_status()
+
+        products = []
+        for product_notes in response.json().get('data'):
+            products.append(self._serialize_product_notes(product_notes))
+
+        return products
 
 
 if __name__ == '__main__':
